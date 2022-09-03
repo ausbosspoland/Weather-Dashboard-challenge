dbff02a313239aa4fad01c76a185042373161b4e
ausbosspoland <austinjpoland311@gmail.com>
2022-09-02 23:31:44 -0500

    finished with JavaScript file and styling sheet, need to add a bit more but so far mvp sort of

---
+++ main.js
@@ -0,0 +1,58 @@
+function GetInfo() {
+
+    var newName = document.getElementById("cityInput");
+    var cityName = document.getElementById("cityName");
+    cityName.innerHTML = "--"+newName.value+"--";
+
+fetch('https://api.openweathermap.org/data/2.5/forecast?q='+newName.value+'&units=imperial&appid=958bf1982262f64b477214a11d1708fc')
+.then(response => response.json())
+.then(data => {
+
+    // trying to get the correct min and max values for each day
+    for(i = 0; i<5; i++){
+        document.getElementById("day" + (i+1) + "Min").innerHTML = "Min: " + Number(data.list[i].main.temp_min + 0).toFixed(1)+ "°";
+    }
+
+    for(i = 0; i<5; i++){
+        document.getElementById("day" + (i+1) + "Max").innerHTML = "Max: " + Number(data.list[i].main.temp_max + 0).toFixed(2) + "°";
+    }
+
+
+    //getting weather Icons
+     for(i = 0; i<5; i++){
+        document.getElementById("img" + (i+1)).src = "http://openweathermap.org/img/wn/"+
+        data.list[i].weather[0].icon
+        +".png";
+    }
+
+    console.log(data)
+
+
+})
+
+.catch(err => alert("Something Happened! Try Checking Your Internet Connection"))
+}
+
+function DefaultScreen(){
+    document.getElementById("cityInput").defaultValue = "Austin";
+    GetInfo();
+}
+
+
+//Getting text for the next five days of the week
+var d = new Date();
+var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday",];
+
+//Function to get the correct integer for the index of the days array
+function CheckDay(day){
+    if(day + d.getDay() > 6){
+        return day + d.getDay() - 7;
+    }
+    else{
+        return day + d.getDay();
+    }
+}
+
+    for(i = 0; i<5; i++){
+        document.getElementById("day" + (i+1)).innerHTML = weekday[CheckDay(i)];
+    }
